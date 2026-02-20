0e4984d27e2a70495308ec27e4781762f74673db
Sudhanshu-kiet <90902873+Sudhanshu-kiet@users.noreply.github.com>
2026-02-21 01:27:38 +0530

    Create token_generator.py

---
+++ token_generator.py
@@ -0,0 +1,22 @@
+import requests
+
+TENANT_ID = "YOUR_TENANT_ID"
+CLIENT_ID = "YOUR_CLIENT_ID"
+CLIENT_SECRET = "YOUR_CLIENT_SECRET"
+
+url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
+
+payload = {
+    "client_id": CLIENT_ID,
+    "scope": "https://graph.microsoft.com/.default",
+    "client_secret": CLIENT_SECRET,
+    "grant_type": "client_credentials"
+}
+
+response = requests.post(url, data=payload)
+
+if response.status_code == 200:
+    print("Access Token Generated Successfully")
+    print(response.json()["access_token"])
+else:
+    print("Error:", response.text)
